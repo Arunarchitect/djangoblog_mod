fedfc797a4c133bad1b84da941da91e79bb89029
Arun <intformbase@gmail.com>
2023-06-02 20:15:57 +0530

    Restrict who can make a blog post

--- theblog/forms.py
+++ theblog/forms.py
@@ -1,26 +1,36 @@
 from django import forms
-from .models import Post
+from .models import Post, Category
+
+# choices = [('interior','interior'),('urban design','urban design'),('landscape','landscape')]
+choices = Category.objects.all().values_list('name','name')
+choice_list = []
+
+for item in choices:
+    choice_list.append(item)
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'author', 'body')
+        fields = ('title', 'title_tag','author', 'category', 'body')
 
         widgets = {
             'title' : forms.TextInput(attrs = {'class': 'form-control'}),
             'title_tag' : forms.TextInput(attrs = {'class': 'form-control'}),
-            'author' : forms.Select(attrs = {'class': 'form-control'}),
+            'author' : forms.TextInput(attrs = {'class': 'form-control', 'value':'', 'id':'star', 'type':'hidden'}),
+            'category' : forms.Select(choices=choice_list, attrs = {'class': 'form-control'}),
+            # 'author' : forms.Select(attrs = {'class': 'form-control'}),
             'body' : forms.Textarea(attrs = {'class': 'form-control'}),
         }
 
 class EditForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag',  'body')
+        fields = ('title', 'title_tag', 'category', 'body')
 
         widgets = {
             'title' : forms.TextInput(attrs = {'class': 'form-control'}),
             'title_tag' : forms.TextInput(attrs = {'class': 'form-control'}),
+            'category' : forms.Select(choices=choice_list, attrs = {'class': 'form-control'}),
             # 'author' : forms.Select(attrs = {'class': 'form-control'}),
             'body' : forms.Textarea(attrs = {'class': 'form-control'}),
         }
